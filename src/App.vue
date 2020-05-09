1418ea2c2faae0ed5cd9321cd9181ec9790ab201
wjq <wjq4416@163.com>
2020-05-09 17:29:14 +0800

    导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,64 @@
 <template>
   <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>-->
+    <div id="nav" class="nav-box">
+      <template v-for="(item,index) in navList">
+        <router-link class="nav-item" :to="item.value" :key="index">
+          <i :class="item.icon"></i>
+          {{ item.name }}
+        </router-link>
+      </template>
+    </div>
+    <div class="container">
     <router-view />
+    </div>
   </div>
 </template>
-
+<script>
+export default {
+  data() {
+    return {
+      navList: [
+        { value: "/", name: "首页", icon: "el-icon-s-home" },
+        { value: "/", name: "期刊介绍", icon: "el-icon-s-order" },
+        { value: "/", name: "编委会", icon: "el-icon-user-solid" },
+        { value: "/", name: "期刊订阅", icon: "el-icon-star-on" },
+        { value: "/", name: "联系我们", icon: "el-icon-phone" }
+      ]
+    };
+  }
+};
+</script>
 <style lang="less">
 @import "assets/css/base";
+</style>
+<style lang="less" scoped>
+.nav-box {
+  background-color: #3aa3f5;
+  width: 1190px;
+  margin: 0 auto;
+  height: 130px;
+  display: flex;
+  border-radius: 10px;
+  .nav-item {
+    width: 20%;
+    display: block;
+    height: 130px;
+    padding: 30px;
+    color: #fff;
+    font-size: 18px;
+    border-radius: 10px;
+    &:hover {
+      background-color: #198feb;
+    }
+    i {
+      color: #fff;
+      display: block;
+      font-size: 50px;
+    }
+  }
+}
+.container{
+  width: 1190px;
+  margin: 0 auto;
+}
 </style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,11 +1,47 @@
 <template>
   <div>
-    <el-container>
-      <el-header>铁道车辆</el-header>
-      <el-main>
-        <div>{{ listData }}</div>
-      </el-main>
-    </el-container>
+    <el-row :gutter="20">
+      <el-col :span="8">
+        <div class="base-panel">
+          <div class="base-panel-header">
+            <span>期刊信息</span>
+            <!-- <el-button type="text">更多</el-button> -->
+          </div>
+          <div class="base-panel-body">
+            <div class="base-line">
+              <div class="base-line-title">主管</div>
+              <div class="base-line-info">中国中车集团有限公司</div>
+            </div>
+            <div class="base-line">
+              <div class="base-line-title">主办</div>
+              <div class="base-line-info">中车青岛四方车辆研究所有限公司</div>
+            </div>
+            <div class="base-line">
+              <div class="base-line-title">编辑出版</div>
+              <div class="base-line-info">《铁道车辆》编辑部</div>
+            </div>
+            <div class="base-line">
+              <div class="base-line-title">主编</div>
+              <div class="base-line-info">陈凯</div>
+            </div>
+            <div class="base-line">
+              <div class="base-line-title">副主编</div>
+              <div class="base-line-info">田玉坤</div>
+            </div>
+            <div class="base-line">
+              <div class="base-line-title">标准编号</div>
+              <div class="base-line-info">ISSN 1001-4632  CN 11-2480/U</div>
+            </div>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="8">
+        <div class></div>
+      </el-col>
+      <el-col :span="8">
+        <div class></div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 <script>
@@ -20,7 +56,7 @@ export default {
     };
   },
   mounted() {
-    this.init();
+    // this.init();
   },
   methods: {
     init() {
@@ -36,4 +72,34 @@ export default {
 </script>
 <style lang="less" scoped>
 // @import "../../assets/css/question";
+.base-panel {
+  .base-panel-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 0;
+    border-bottom: 2px solid #eee;
+    span {
+      font-size: 28px;
+    }
+  }
+  .base-panel-body {
+    padding: 10px 0;
+  }
+}
+.base-line {
+  text-align: left;
+  display: flex;
+  border-bottom: 1px dotted #eee;
+  line-height: 28px;
+  font-size: 14px;
+  .base-line-title {
+    color: #909399;
+    // width: 80px;
+    flex-basis: 80px;
+  }
+  .base-line-info {
+    color: #303133;
+  }
+}
 </style>
